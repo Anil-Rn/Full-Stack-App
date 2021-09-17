8e84078f8e5e4b07a94ee8f030cee1e9330741cd
Anil-Rn <87747537+Anil-Rn@users.noreply.github.com>
2021-09-17 14:18:47 +0530

    Lambda code in Python to write into dynamoDB

--- LambdaToDB.py
+++ LambdaToDB.py
@@ -8,7 +8,7 @@ from time import gmtime, strftime
 # create a DynamoDB object using the AWS SDK
 dynamodb = boto3.resource('dynamodb')
 # use the DynamoDB object to select our table
-table = dynamodb.Table('HelloWorldDatabase')
+table = dynamodb.Table('MyDatabase')
 
 # define the handler function that the Lambda service will use as an entry point
 def lambda_handler(event, context):
